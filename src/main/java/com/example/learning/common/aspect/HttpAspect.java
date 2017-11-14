021b22b221727b2d7d1f688241281f0a94f8fb03
zhaoqicheng <643198195@qq.com>
2017-11-14 16:53:10 +0800

    AOP 示例代码解释，未来打算用来结合用户登录，实现JWT规范，完成JWT规范代码

--- src/main/java/com/example/learning/common/aspect/HttpAspect.java
+++ src/main/java/com/example/learning/common/aspect/HttpAspect.java
@@ -10,6 +10,17 @@ import org.springframework.stereotype.Component;
  *
  * 拦截器
  * 拦截方法防止用户未登录访问不可访问方法
+ *
+ * 实现AOP的切面主要有以下几个要素：
+
+     使用@Aspect注解将一个java类定义为切面类
+     使用@Pointcut定义一个切入点，可以是一个规则表达式，比如下例中某个package下的所有函数，也可以是一个注解等。
+     根据需要在切入点不同位置的切入内容
+     使用@Before在切入点开始处切入内容
+     使用@After在切入点结尾处切入内容
+     使用@AfterReturning在切入点return内容之后切入内容（可以用来对处理返回值做一些加工处理）
+     使用@Around在切入点前后切入内容，并自己控制何时执行切入点自身的内容
+     使用@AfterThrowing用来处理当切入内容部分抛出异常之后的处理逻辑
  */
 @Aspect
 @Component
